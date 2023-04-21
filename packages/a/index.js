d234ce5060b4dcba508ec8b59b2bbfc533fde3be
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2023-04-21 10:00:42 +0800

    feat: add console.log in package a

--- packages/a/index.js
+++ packages/a/index.js
@@ -2,3 +2,4 @@ var pjson = require('./package.json');
 console.log(pjson.version);
 console.log('pnpm version minor from 1.0.0 to 1.1.0')
 console.log('pnpm version minor from 1.1.0 to 1.2.0')
+console.log('pnpm version minor from 1.2.0 to 1.3.0')
